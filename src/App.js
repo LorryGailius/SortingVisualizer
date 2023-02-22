120413fa3f14b6ed61f22e1af59544427859ea88
Laurynas <lauris.gailius@gmail.com>
2023-02-22 23:29:20 +0200

    Added BubbleSort and SelectionSort

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
+import { Button, FormControl, InputLabel, MenuItem, Select, Slider, Toolbar, Typography } from '@mui/material';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer'
 
 function App() {
+  const [arraySize, setArraySize] = useState(100);
+  const [speed, setSpeed] = useState(1000);
+  const [reset, setReset] = useState(false);
+  const [selection, setSelection] = useState(0);
+
+  const changeSize = (event, value) => {
+    setArraySize(value);
+  };
+
+  const changeSelection = (event, value) => {
+    setSelection(event.target.value);
+  };
+
+  const changeSpeed = (event, value) => {
+    setSpeed(value);
+  };
+
+  const handleSort = () => {
+    switch (selection) {
+      case 0:
+        sortingVisualizerRef.current.bubbleSort();
+        break;
+
+      case 1:
+        sortingVisualizerRef.current.selectionSort();
+        break;
+
+      default:
+        break;
+    }
+
+  };
+
+  const handleReset = () => {
+    setReset(!reset);
+  };
+
+  const sortingVisualizerRef = useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Toolbar sx={{ backgroundColor: '#1d2025', display: 'flex', alignItems: 'center', justifyContent: 'center' }} disableGutters>
+        <Button
+          onClick={handleReset}
+          sx={{ height: '2rem', }}
+          variant="contained">Generate new array</Button>
+
+
+        <div className='sliders'>
+          <Typography variant='body2' color={'white'} align="justify" sx={{ margin: '0 0 -0.5rem 0' }}>
+            Array size
+          </Typography>
+          <Slider
+            value={arraySize}
+            onChange={changeSize}
+            step={10}
+            min={4}
+            max={140}
+          />
+        </div>
+
+        <FormControl>
+          <InputLabel sx={{ color: 'white' }}>Algorithm</InputLabel>
+          <Select
+            value={selection}
+            label="Algorithm"
+            onChange={changeSelection}
+            sx={{
+              height: '2rem',
+              color: 'white',
+            }}
+          >
+            <MenuItem value={0}>Bubble Sort</MenuItem>
+            <MenuItem value={1}>Selection Sort</MenuItem>
+          </Select>
+        </FormControl>
+
+        <div className='sliders'>
+          <Typography variant='body2' color={'white'} align="justify" sx={{ margin: '0 0 -0.5rem 0' }}>
+            Sorting speed
+          </Typography>
+          <Slider
+            value={speed}
+            onChange={changeSpeed}
+            min={0}
+            max={1000}
+          />
+        </div>
+
+        <Button
+          onClick={handleSort}
+          sx={{ height: '2rem', }}
+          variant="contained">Sort</Button>
+      </Toolbar>
+
+      <SortingVisualizer
+        size={arraySize}
+        reset={reset}
+        speed={speed}
+        ref={sortingVisualizerRef} />
     </div>
   );
 }
---
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import './SortingVisualizer.css';
+
+export default class SortingVisualizer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      array: [],
+      size: props.size,
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.reset !== this.props.reset) {
+      this.resetArray();
+    }
+
+    if (prevProps.size !== this.props.size) {
+      this.resetArray();
+    }
+  }
+
+  resetArray() {
+    const array = [];
+    for (let index = 0; index < this.props.size; index++) {
+      array.push(randomIntFromInterval(20, 600));
+
+    }
+
+    const bars = document.getElementsByClassName("array-bar");
+    for (let i = 0; i < bars.length; i++) {
+      bars[i].classList.remove("higher-val", "lower-val", "compare-val");
+    }
+
+    this.setState({ array });
+  }
+
+  render() {
+    const { array } = this.state;
+
+    return (
+      <div className="array-container">
+        {array.map((value, idx) => (
+          <div className="array-bar"
+            key={idx}
+            style={{
+              height: `${value}px`,
+              width: `calc(100% / ${array.length} - 2px)`,
+            }}>
+            {array.length <= 50 && (<span>{value}</span>)}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  async bubbleSort() {
+    const { array } = this.state;
+    const n = array.length;
+
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        const bars = document.getElementsByClassName("array-bar");
+        bars[j].classList.add("compare-val");
+
+        if (array[j] > array[j + 1]) {
+          bars[j].classList.remove("compare-val", "higher-val", "lower-val");
+          bars[j].classList.add("lower-val");
+          bars[j + 1].classList.add("compare-val");
+          const temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+          this.setState({ array: array });
+
+        }
+        else {
+          bars[j].classList.remove("compare-val", "higher-val", "lower-val");
+          bars[j].classList.add("higher-val");
+          bars[j + 1].classList.add("compare-val");
+        }
+        await sleep(calcSpeedMs(this.props.speed));
+        bars[j].classList.remove("compare-val", "higher-val", "lower-val");
+        bars[j + 1].classList.remove("compare-val", "higher-val", "lower-val");
+      }
+    }
+    this.checkSort();
+  }
+
+  async selectionSort() {
+
+    const { array } = this.state;
+    const n = array.length;
+
+    for (let i = 0; i < n; i++) {
+      const bars = document.getElementsByClassName("array-bar");
+      bars[i].classList.add("compare-val");
+      let lowest = i
+      for (let j = i + 1; j < n; j++) {
+        if (array[j] < array[lowest]) {
+          lowest = j;
+          bars[j].classList.add("lower-val");
+        }
+        else {
+          bars[j].classList.add("higher-val");
+        }
+        await sleep(calcSpeedMs(this.props.speed));
+
+        bars[j].classList.remove("compare-val", "higher-val", "lower-val");
+      }
+      if (lowest !== i) {
+        ;[array[i], array[lowest]] = [array[lowest], array[i]]
+      }
+      this.setState({ array: array });
+
+      bars[i].classList.remove("compare-val", "higher-val", "lower-val");
+    }
+    this.checkSort();
+  }
+
+
+  async checkSort() {
+    const bars = document.getElementsByClassName("array-bar");
+    const { array } = this.state;
+    for (let i = 0; i < array.length - 1; i++) {
+      const currBar = bars[i];
+      const nextBar = bars[i + 1];
+      if (array[i] > array[i + 1]) {
+        currBar.classList.add("higher-val");
+        nextBar.classList.add("higher-val");
+      }
+      await sleep(0);
+    }
+    for (let i = 0; i < array.length; i++) {
+      bars[i].classList.add("lower-val");
+      await sleep(0);
+    }
+  }
+
+
+}
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function calcSpeedMs(x) {
+  return 1000 - x;
+}
